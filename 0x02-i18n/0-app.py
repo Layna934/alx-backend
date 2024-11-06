5e722cb720b22d92fbe798e8a3c94ca668db8255
Precious <eseprecious934@gmail.com>
2024-11-06 00:21:18 +0000

    basic flask doc

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python3
+
+"""
+Basic flask app
+"""
 from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def home():
+    """
+    Function to render template
+    """
     return render_template('0-index.html')
