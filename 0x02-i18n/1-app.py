dd955d25e0e97e3ab732da405d5f9c5ec28374c8
Precious <eseprecious934@gmail.com>
2024-11-06 00:48:59 +0000

    basic babel

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""
+Basic Babel Setup
+"""
+from flask import Flask, render_template
+from flask_babel import Babel
+
+app = Flask(__name__)
+babel = Babel(app)
+
+class Config():
+    """
+    This class is used to configure Babel
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+
+@app.route('/')
+def home():
+    """
+    This is a function to render template
+    """
+    return render_template('1-index.html')
+
+if __name__ == '__main__':
+    app.run(host:"0.0.0.0", port:5000)
